f503d00e109432d2561222b07a3d8ad0c5782d53
Griffin Mullins <griffin.mullins@capitalone.com>
2016-10-16 09:59:32 -0500

    Initial Commit

---
+++ tumblrHW4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tumblrHW4
+//
+//  Created by Mullins, Griffin on 10/16/16.
+//  Copyright © 2016 Mullins, Griffin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
